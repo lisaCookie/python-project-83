179dd5cb5efd25984ebd6d8310d863cf58dfc893
lisa <lisavetabelova@gmail.com>
2025-08-24 02:21:42 +0300

    modified:   Dockerfile         modified:   page_analyzer/__pycache__/__init__.cpython-312.pyc         modified:   page_analyzer/__pycache__/app.cpython-312.pyc         modified:   page_analyzer/__pycache__/app.cpython-313.pyc         modified:   page_analyzer/app.py         modified:   pyproject.toml         modified:   uv.lock

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,12 @@ FROM python:3.12-slim
 
 WORKDIR /app
 
+COPY pyproject.toml .
+
+
+RUN pip install --no-cache-dir --upgrade pip && \
+    pip install --no-cache-dir .
+
 COPY . .
-
-RUN pip install --no-cache-dir --upgrade pip
-
-RUN pip install --no-cache-dir poetry
-RUN poetry config virtualenvs.create false && poetry install --no-interaction --no-ansi --no-root
 
 CMD ["gunicorn", "page_analyzer.app:app", "--bind", "0.0.0.0:8000"]
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,10 @@
-# app.py
+
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash
+
 import psycopg2
 import validators
-from urllib.parse import urlparse
 from dotenv import load_dotenv
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 load_dotenv()
 
@@ -14,9 +14,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 app = Flask(__name__)
 app.secret_key = SECRET_KEY 
 
+
 def get_db_connection():
     conn = psycopg2.connect(DATABASE_URL)
     return conn
+
 
 @app.route('/', methods=['GET', 'POST'], endpoint='home')
 def index():
@@ -37,12 +39,15 @@ def index():
         conn = get_db_connection()
         with conn:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT id FROM urls WHERE name = %s", (url_input,))
+                cursor.execute(
+                    "SELECT id FROM urls WHERE name = %s", 
+                    (url_input,)
+                )
                 existing = cursor.fetchone()
                 if existing:
                     flash('Этот URL уже добавлен', 'info')
                     return redirect(url_for('url_detail', id=existing[0]))
-                # Добавляем новый URL
+                # Добавить новый URL
                 cursor.execute(
                     "INSERT INTO urls (name) VALUES (%s) RETURNING id",
                     (url_input,)
@@ -52,21 +57,31 @@ def index():
         return redirect(url_for('url_detail', id=new_id))
     return render_template('index.html')
 
+
 @app.route('/urls', endpoint='urls')
 def all_urls():
     conn = get_db_connection()
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT id, name, created_at FROM urls ORDER BY created_at DESC")
+            cursor.execute(
+                "SELECT id, name, created_at FROM urls "
+                "ORDER BY created_at DESC"
+            )
+
             urls = cursor.fetchall()
     return render_template('urls.html', urls=urls)
+
 
 @app.route('/urls/<int:id>')
 def url_detail(id):
     conn = get_db_connection()
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT id, name, created_at FROM urls WHERE id = %s", (id,))
+            cursor.execute(
+                "SELECT id, name, created_at FROM urls WHERE id = %s", 
+                (id,)
+            )
+
             url_record = cursor.fetchone()
     if url_record:
         return render_template('url_detail.html', url=url_record)
